ada04987e8a591c86a28382eddf481c755c047e9
Suelen <suelenonyrius@gmail.com>
2021-11-14 21:20:06 -0300

    segunda tentiva - git push - soluciona  decimo terceiro  desafio

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,46 +1,15 @@
 // Desafio 10
-function techList() {
-//   // seu código aqui
-//   let result = []
-//   let tech2 = tech
-//    let objectTechList =  {
-    
-//   }
-//   if (tech ===undefined || name ===undefined) {
-//     ("Vazio!")
-//   } else {
-//   tech2.sort()
-//   for(let index = 0; index < tech2.length; index +=1)
-//   objectTechList.tech = tech2[index]
-//     Object.assign(objectTechList,objectTechList.tech )
-//   }
+function techList(tech, name) {
+// seu código aqui
   
-// (objectTechList)
-
-}
-
-
-//  techList("Suelen")
-// techList( ["aaa", "bbbb"],"Suelen")
-
-
-
-/**Source https://www.todoespacoonline.com/w/2014/04/array-em-javascript/ */
-/**Source https://www.ti-enxame.com/pt/javascript/javascript-qual-e-melhor-maneira-de-converter-array-em-objeto/830138339/ */
-/**Source https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer */
-/**Source https://stackoverflow.com/questions/54380425/create-new-object-from-value-of-another-object-in-js */
-/**Source https://codezup.com/5-ways-to-convert-array-of-objects-to-object-in-javascript/ */
+  
+  }
+  
+  
 
 // Desafio 11
 function generatePhoneNumber(numberPhone) {
-  // seu código aqui
-  // let count = 0
-  // if (numberPhone.length != 11) {
-  //   return "Array com tamanho incorreto."
-  // };for (let index = 0; index < numberPhone.length; index +=1) {
-  //    for (let equal = numberPhone[index]; equal ===  )
-  // }
-  // }
+
 }
 
 /**Source https://dev.to/huyddo/find-duplicate-or-repeat-elements-in-js-array-3cl3 */
